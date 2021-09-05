a9589281fee032e9b3f07ebab2ee97dd98f8f29e
Rbr <roberto.ribeiro@al.infnet.edu.br>
2021-09-05 18:13:01 -0300

    Header e Footer  prontos

--- web/src/Components/Header/index.js
+++ web/src/Components/Header/index.js
@@ -6,7 +6,11 @@ function Header(){
     return (
         
         <Styled.Container>
-          
+            <Styled.Menu>
+                <a href="">INÍCIO</a>
+                    <span className="barra"  />
+                <a href="">TAREFA</a>
+            </Styled.Menu>
         </Styled.Container>
 
     )
