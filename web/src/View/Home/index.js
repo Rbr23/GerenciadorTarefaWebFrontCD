b40b4bc0a0069aeefcdc9cb59c8a4fbbbb4266ab
Rbr <roberto.ribeiro@al.infnet.edu.br>
2021-09-19 18:48:05 -0300

    implementação do useStated

--- web/src/View/Home/index.js
+++ web/src/View/Home/index.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as Styled from './styles';
-import Header from '../../Components/Header'
-import Footer from '../../Components/Footer'
-import Filter from '../../Components/Filter'
+import Header from '../../Components/Header';
+import Footer from '../../Components/Footer';
+import Filter from '../../Components/Filter';
+import Imagem from '../../Imgs/filter.png';
+
 
 function Home() {
+
+  const [filterActive, functionFilter] = useState();
+
   return (
     <Styled.Container>
       <Header />
         <Styled.ContainerFilter>
-          <Filter title="Todos"/>
-          <Filter title="Hoje"/>
-          <Filter title="Semana"/>
-          <Filter title="Mês"/>
+          <button type='button' onClick={() => functionFilter('todos')}>
+          <Filter title="Todos" img={Imagem} actived={filterActive == 'todos'}/>
+          </button>
+
+          <button type='button' onClick={() => functionFilter('hoje')}>
+          <Filter title="Hoje" img={Imagem} actived={filterActive == 'hoje'}/>
+          </button>
+
+          <button type='button' onClick={() => functionFilter('semana')}>
+          <Filter title="Semana" img={Imagem} actived={filterActive == 'semana'}/>
+          </button>
+
+          <button type='button' onClick={() => functionFilter('mes')}>
+          <Filter title="Mês" img={Imagem} actived={filterActive == 'mes'}/>
+          </button>
+          
+          
+          
+          
         </Styled.ContainerFilter>
       <Footer />
     </Styled.Container>
