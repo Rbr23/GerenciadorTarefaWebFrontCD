8461f08d5f3963b43657e63ca9fd95154d6b1232
Rbr23 <44401064+Rbr23@users.noreply.github.com>
2021-11-03 10:18:37 -0300

    trazendo imagens e data dinamicamente

--- web/src/View/Home/index.js
+++ web/src/View/Home/index.js
@@ -5,14 +5,7 @@ import api from '../../services/api';
 
 /* IMAGENS */
 import Imagem from '../../Imgs/filter.png';
-import futebol from '../../Imgs/futebol.jpg';
-import viagem from '../../Imgs/viagem.png';
-import cinema from '../../Imgs/cinema.png';
-import lanche from '../../Imgs/lanche.jpg';
-import prova from '../../Imgs/prova.jpg';
-import academia from '../../Imgs/academia.jpg';
-import compra from '../../Imgs/compra.jpg';
-import trabalho from '../../Imgs/trabalho.png';
+
 
 
 
@@ -69,8 +62,8 @@ function Home() {
          <Styled.ContainerCard>
            {
              tasks.map(
-               t => ( 
-               <TaskCard />
+               t =>(  
+               <TaskCard type={t.type} title={t.title} when={t.when} />
                ))
            }
          </Styled.ContainerCard>
